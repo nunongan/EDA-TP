7cc3f2adbf64128de9d4f7b82827f737069ce7a8
Nuno Ngan <nunongan@gmail.com>
2022-03-27 17:15:14 +0100

    Update #4
    - Removed some header files
    
    - Coded "Change Operation"
    
    - Coded "Detemine Maximum Time in each Operation"
    
    - Coded "Detemine Minimum Time in each Operation"
    
    - Coded "Detemine Average Time in each Operation"
    
    TO DO:
    
    Menu
    
    Remove Operation
    
    Comment Code

--- main.c
+++ main.c
@@ -4,8 +4,6 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
-#include <unistd.h>
-#include <stdbool.h>
 
 #pragma endregion
 
@@ -23,12 +21,14 @@ typedef struct _operation
 {
 
     int operationID;
-    struct _operation *next;
+    struct _operation *next, *prev;
     struct _production *first, *last;
 
 } Operation;
 
 #pragma endregion
+
+#pragma region headInsert
 
 Operation *head_insert(Operation *list, int operationID)
 {
@@ -41,76 +41,85 @@ Operation *head_insert(Operation *list, int operationID)
     return new;
 }
 
+#pragma endregion
+
+#pragma region tailInsert
+
 Production *tail_insert(Production *list, int machineID, int time)
 {
 
-    Production *cell = (Production *)malloc(sizeof(Production));
+    Production *new = (Production *)malloc(sizeof(Production));
 
-    cell->machineID = machineID;
-    cell->time = time;
-    cell->next = NULL;
-    cell->prev = list;
-    if (cell->prev)
+    new->machineID = machineID;
+    new->time = time;
+    new->next = NULL;
+    new->prev = list;
+    if (new->prev)
     {
-        cell->prev->next = cell;
+        new->prev->next = new;
     }
 
-    return cell;
+    return new;
 }
 
+#pragma endregion
+
+#pragma region insertion
 Operation *insertion(Operation *list, int operationID, int machineID, int time)
 {
 
-    Operation *aux = list;
+    Operation *point = list;
 
-    while (aux)
+    while (point)
     {
 
-        if (aux->operationID == operationID)
+        if (point->operationID == operationID)
         {
             break;
         }
 
-        aux = aux->next;
+        point = point->next;
     }
 
-    // if(!aux) return list;
-
-    if (!aux->last) // nobody in the waiting line
+    if (!point->last)
     {
-        aux->first = aux->last = tail_insert(aux->last, machineID, time);
+        point->first = point->last = tail_insert(point->last, machineID, time);
     }
     else
     {
-        aux->last = tail_insert(aux->last, machineID, time);
+        point->last = tail_insert(point->last, machineID, time);
     }
     return list;
 }
+
+#pragma endregion
+
+#pragma region fileManipulation
 
 void saveData(Operation *list)
 {
 
     FILE *file;
 
-    file = fopen("./data/productions_tmp.txt", "w+");
+    file = fopen("./data/productionsWrite.txt", "w+");
 
     for (; list; list = list->next)
     {
 
-        Production *ptr = list->first;
+        Production *point = list->first;
 
-        for (; ptr;)
+        for (; point;)
         {
 
-            fprintf(file, "%d,%d,%d\n", list->operationID, ptr->machineID, ptr->time);
-            ptr = ptr->next;
+            fprintf(file, "%d,%d,%d\n", list->operationID, point->machineID, point->time);
+            point = point->next;
         }
     }
 
     fclose(file);
 }
 
-Operation *readData(Operation *list)
+Operation *loadData(Operation *list)
 {
     FILE *file;
 
@@ -122,7 +131,6 @@ Operation *readData(Operation *list)
         char line[1000];
         int operationID, machineID, time;
         fgets(line, 1000, file);
-        printf("%s",line);
         sscanf(line, "%d,%d,%d", &operationID, &machineID, &time);
         list = head_insert(list, operationID);
         list = insertion(list, operationID, machineID, time);
@@ -133,12 +141,157 @@ Operation *readData(Operation *list)
     fclose(file);
 }
 
+#pragma endregion
+
+#pragma region Functions
+
+Operation *changeOperation(Operation *list, int operationID, int machineID, int time)
+{
+    for (; list; list = list->next)
+    {
+        if (list->operationID == operationID)
+        {
+            Production *point = list->first;
+
+            for (; point;)
+            {
+                if (point->machineID == machineID)
+                {
+
+                    point->time = time;
+                }
+                point = point->next;
+            }
+        }
+    }
+    return list;
+}
+
+Operation *minimumTime(Operation *list)
+{
+    int min[50];
+    for (int i = 0; i < 50; i++)
+    {
+
+        min[i] = 9999;
+    }
+    for (; list; list = list->next)
+    {
+        Production *point = list->first;
+        for (; point;)
+        {
+            if (min[list->operationID] > point->time)
+            {
+
+                min[list->operationID] = point->time;
+            }
+
+            point = point->next;
+        }
+    }
+    for (int i = 0; i < 50; i++)
+    {
+        if (min[i] != 9999)
+        {
+            printf("%d -> %d \n", i, min[i]);
+        }
+    }
+    return list;
+}
+
+Operation *maximumTime(Operation *list)
+{
+    int max[50];
+    for (int i = 0; i < 50; i++)
+    {
+
+        max[i] = 0;
+    }
+    for (; list; list = list->next)
+    {
+        Production *point = list->first;
+        for (; point;)
+        {
+            if (max[list->operationID] < point->time)
+            {
+
+                max[list->operationID] = point->time;
+            }
+
+            point = point->next;
+        }
+    }
+    for (int i = 0; i < 50; i++)
+    {
+        if (max[i] != 0)
+        {
+            printf("%d -> %d \n", i, max[i]);
+        }
+    }
+    return list;
+}
+
+Operation *avg(Operation *list)
+{
+    int sum[50], counter[50];
+
+    for (int i = 0; i < 50; i++)
+    {
+        counter[i] = 0;
+        sum[i] = 0;
+    }
+
+    for (; list; list = list->next)
+    {
+        Production *point = list->first;
+        for (; point;)
+        {
+
+            sum[list->operationID] += point->time;
+            counter[list->operationID]++;
+
+            point = point->next;
+        }
+    }
+    for (int i = 0; i < 50; i++)
+    {
+        if (counter[i] != 0)
+        {
+            float average = sum[i] / (float)counter[i];
+            printf("%d -> %.2f \n", i, average);
+        }
+    }
+    return list;
+}
+
+void showList(Operation *list)
+{
+
+    for (; list; list = list->next)
+    {
+
+        Production *point = list->first;
+
+        for (; point;)
+        {
+
+            printf("%d | %d | %d\n", list->operationID, point->machineID, point->time);
+            point = point->next;
+        }
+    }
+}
+
+
+#pragma endregion
+
 int main()
 {
 
     Operation *test = NULL;
 
-    test = readData(test);
-    saveData(test);
+    test = loadData(test);
+    avg(test);
+    showList(test);
+
     return 0;
 }
